bc5787addca336c53345560c9a4f8a475be384e1
Koen Winkel <2003KOWI@gmail.com>
2024-10-04 10:13:06 +0200

    Added Helper method axis to vector

--- BlazeFrame/Maths/MathHelper.cs
+++ BlazeFrame/Maths/MathHelper.cs
@@ -5,4 +5,15 @@ public static class MathHelper
     public static float Determinant3x3(float a, float b, float c, float d, float e, float f, float g, float h, float i) =>
         a * (e * i - f * h) - b * (d * i - f * g) + c * (d * h - e * g);
 
+    public static Vector3 GetDirection(this Axis axis)
+    {
+        return axis switch
+        {
+            Axis.X => Vector3.UnitX,
+            Axis.Y => Vector3.UnitY,
+            Axis.Z => Vector3.UnitZ,
+            _ => throw new ArgumentOutOfRangeException(nameof(axis), axis, null)
+        };
+    }
+
 }
