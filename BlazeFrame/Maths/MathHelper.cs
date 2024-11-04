8b37daac67902516c348e61eccb339cb2a5c5e99
Koen Winkel <2003KOWI@gmail.com>
2024-11-04 10:46:49 +0100

    Added Matrix4 Construction

--- BlazeFrame/Maths/MathHelper.cs
+++ BlazeFrame/Maths/MathHelper.cs
@@ -16,4 +16,30 @@ public static class MathHelper
         };
     }
 
+    public static Matrix4 CreateOrthographic(float left, float right, float top, float bottom, float near, float far)
+    {
+        var result = Matrix4.Identity;
+        result.M11 = 2f / (right - left);
+        result.M22 = 2f / (top - bottom);
+        result.M33 = -2f / (far - near);
+        result.M41 = -(right + left) / (right - left);
+        result.M42 = -(top + bottom) / (top - bottom);
+        result.M43 = -(far + near) / (far - near);
+        return result;
+    }
+
+    public static Matrix4 CreatePerspective(float fov, float aspect, float near, float far) 
+    {
+        var result = Matrix4.Identity;
+        var tanHalfFov = MathF.Tan(fov / 2f);
+
+        result.M11 = 1f / (aspect * tanHalfFov);
+        result.M22 = 1f / tanHalfFov;
+        result.M33 = -(far + near) / (far - near);
+        result.M34 = -(2f * far * near) / (far - near);
+        result.M43 = -1f;
+        result.M44 = 0f;
+        return result;
+    }
+
 }
